86395fafa22f9f2b6aad0e4d42f669f3909acfd0
intellltech <danieljames.maxdev@gmail.com>
2022-12-17 13:18:19 +0700

    add models

---
+++ sequelize/models/index.js
@@ -0,0 +1,34 @@
+// @ts-check
+'use strict'
+
+const Sequelize = require('sequelize')
+
+const models = [
+  require('./Block'),
+  require('./Transaction'),
+  require('./Identifier'),
+  require('./ChangeControllerMessage'),
+  require('./SetAttributeMessage'),
+  require('./RevokeAttributeMessage'),
+  require('./Document'),
+  require('./Controller'),
+]
+
+const env = process.env.NODE_ENV || 'development'
+const config = require('../config')[env]
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
+
+models.map(model => {
+  model.initWithSequelizeClient(sequelize)
+
+  return model
+})
+
+models.map(model => model.postInit())
+
+const db = {}
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
