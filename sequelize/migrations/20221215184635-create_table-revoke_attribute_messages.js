f4803b7120aea87b4d1f7b3fc9b08df2856747bf
intellltech <danieljames.maxdev@gmail.com>
2022-12-18 21:17:32 +0700

    update migrations

--- sequelize/migrations/20221215184635-create_table-revoke_attribute_messages.js
+++ sequelize/migrations/20221215184635-create_table-revoke_attribute_messages.js
@@ -3,7 +3,7 @@
 const TABLE_NAME = 'revoke_attribute_messages'
 const COLUMN_NAME = {
   TRANSACTION_ID: 'transaction_id',
-  IDENTIFIER_ID: 'identifier_id',
+  IDENTIFIER: 'identifier',
   NAME: 'name',
   VALUE: 'value',
 }
@@ -27,9 +27,9 @@ module.exports = {
         field: COLUMN_NAME.TRANSACTION_ID,
         allowNull: false,
       },
-      identifierId: {
-        type: Sequelize.BIGINT,
-        field: COLUMN_NAME.IDENTIFIER_ID,
+      identifier: {
+        type: Sequelize.STRING(191),
+        field: COLUMN_NAME.IDENTIFIER,
         allowNull: false,
       },
       name: {
@@ -65,11 +65,11 @@ module.exports = {
         ].join('_'),
       }),
       queryInterface.addIndex(TABLE_NAME, [
-        COLUMN_NAME.IDENTIFIER_ID,
+        COLUMN_NAME.IDENTIFIER,
       ], {
         name: [
           TABLE_NAME,
-          COLUMN_NAME.IDENTIFIER_ID,
+          COLUMN_NAME.IDENTIFIER,
           'index',
         ].join('_'),
       }),
