7a946b277136ea92d408f5c3393a8e534ffafbdb
intellltech <danieljames.maxdev@gmail.com>
2022-12-19 23:24:43 +0700

    add trial exporter

--- trials/exporter.js
+++ trials/exporter.js
@@ -8,12 +8,7 @@ const Exporter = require('../app/exporter/Exporter')
 const main = async () => {
   const granoDidClient = await GranoDidClient.createFulfilled()
   const exporter = Exporter.create({ granoDidClient: granoDidClient })
-  // const block = await exporter.fetchLatestBlockFromNode()
-  // console.log(block)
-  // const txs = await exporter.fetchTransactionsByHeight(10000000)
-  // console.log(txs)
-  //
-  await exporter.sync(10000000)
+  await exporter.sync(5239)
 }
 
 main()
