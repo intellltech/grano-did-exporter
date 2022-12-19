ffc0a24687a99ed8c4c1a884c447aed5b0417874
intellltech <danieljames.maxdev@gmail.com>
2022-12-20 01:24:06 +0700

    add trial process

---
+++ trials/repeatProcess.js
@@ -0,0 +1,14 @@
+// @ts-check
+'use strict'
+
+const { GranoDidClient } = require('@eg-easy/grano-did-client')
+
+const Exporter = require('../app/exporter/Exporter')
+
+const main = async () => {
+  const granoDidClient = await GranoDidClient.createFulfilled()
+  const exporter = Exporter.create({ granoDidClient: granoDidClient })
+  await exporter.repeatProcess()
+}
+
+main()
