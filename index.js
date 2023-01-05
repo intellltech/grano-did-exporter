b1f3336f58fa6323998fbfe3a3c21b64ae39543a
intellltech <danieljames.maxdev@gmail.com>
2023-01-05 10:20:50 +0900

    rm DatabaseClient

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // @ts-check
 'use strict'
 
-const DatabaseClient = require('./src/app/client/DatabaseClient')
 const GranoDidExporter = require('./src/app/exporter/GranoDidExporter')
 
 module.exports = {
-  DatabaseClient,
   GranoDidExporter,
 }
